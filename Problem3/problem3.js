b2b6bb45d2e9eb116dadec8bfe9153db418e6a89
pratham1324098 <94662966+pratham1324098@users.noreply.github.com>
2024-04-15 00:11:58 +0530

    Updated problem3.js

--- Problem3/problem3.js
+++ Problem3/problem3.js
@@ -4,11 +4,10 @@ function sortCarModelsAlphabetically(inventory) {
         carModels.push(inventory[i].car_model);
     }
 
-    // Sort the car models alphabetically
+
     for (let i = 0; i < carModels.length - 1; i++) {
         for (let j = i + 1; j < carModels.length; j++) {
             if (carModels[i] > carModels[j]) {
-                // Swap elements if they are in the wrong order
                 const temp = carModels[i];
                 carModels[i] = carModels[j];
                 carModels[j] = temp;
