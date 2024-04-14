7c2e8ba78ae46fc82671d93367c945b3115412ca
pratham1324098 <94662966+pratham1324098@users.noreply.github.com>
2024-04-15 00:12:30 +0530

    Updated problem4.js

--- Problem4/problem4.js
+++ Problem4/problem4.js
@@ -1,6 +1,5 @@
 
 function getCarYears(inventory) {
-    // const years = inventory.map(car => car.car_year);
     const years =[];
     for(let i=0;i<inventory.length;i++){
         years.push(inventory[i].car_year);
